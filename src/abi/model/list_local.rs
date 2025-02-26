9dfb606d421c42f90eda28f824711ef237da3079
stackoverflower <bee0_0@icloud.com>
2025-02-26 22:15:52 +0800

    fix(model): fix families in ModelInfoDetail

--- src/abi/model/list_local.rs
+++ src/abi/model/list_local.rs
@@ -25,7 +25,7 @@ pub struct ModelInfo {
 pub struct ModelInfoDetail {
     pub format: String,
     pub family: String,
-    pub families: Option<String>,
+    pub families: Option<Vec<String>>,
     pub parameter_size: String,
     pub quantization_level: String,
 }
