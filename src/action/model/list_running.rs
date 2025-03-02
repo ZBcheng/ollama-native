8172cf3ff77fb09e2c35688dee3891bc627d6969
stackoverflower <bee0_0@icloud.com>
2025-03-02 21:01:02 +0800

    refector: refectored ListRunningModels.into_future

--- src/action/model/list_running.rs
+++ src/action/model/list_running.rs
@@ -1,11 +1,12 @@
 use std::marker::PhantomData;
 
 use futures::future::BoxFuture;
+use reqwest::StatusCode;
 
 use crate::{
     abi::model::list_running::{ListRunningModelsRequest, ListRunningModelsResponse},
-    action::{Action, OllamaClient},
-    error::OllamaError,
+    action::{Action, OllamaClient, parse_response},
+    error::{OllamaError, ServerError},
 };
 
 impl Action<ListRunningModelsRequest, ListRunningModelsResponse> {
@@ -25,11 +26,13 @@ impl IntoFuture for Action<ListRunningModelsRequest, ListRunningModelsResponse> 
     fn into_future(self) -> Self::IntoFuture {
         Box::pin(async move {
             let reqwest_resp = self.ollama.get(&self.request).await?;
-            let response = reqwest_resp
-                .json()
-                .await
-                .map_err(|e| OllamaError::DecodingError(e))?;
-            Ok(response)
+            match reqwest_resp.status() {
+                StatusCode::OK => parse_response(reqwest_resp).await,
+                _ => {
+                    let error: ServerError = parse_response(reqwest_resp).await?;
+                    Err(OllamaError::ServerError(error.error))
+                }
+            }
         })
     }
 }
