a8c562022ba8bf26a1279af48b58b49f8d21b5ec
stackoverflower <bee0_0@icloud.com>
2025-03-02 19:25:48 +0800

    feat: add feature flags for OllamaError

--- src/error.rs
+++ src/error.rs
@@ -8,24 +8,26 @@ pub enum OllamaError {
     #[error("decoding error: {0}")]
     DecodingError(reqwest::Error),
 
+    #[cfg(feature = "stream")]
     #[error("stream decoding error: {0}")]
     StreamDecodingError(String),
-
-    #[error("feature not available: {0}")]
-    FeatureNotAvailable(String),
 
     #[error("invalid format: {0}")]
     InvalidFormat(String),
 
+    #[cfg(feature = "model")]
     #[error("model does not exist")]
     ModelDoesNotExist,
 
+    #[cfg(feature = "model")]
     #[error("blob does not exist")]
     BlobDoesNotExist,
 
+    #[cfg(feature = "model")]
     #[error("unexpected digest")]
     UnexpectedDigest,
 
+    #[cfg(feature = "model")]
     #[error("file error: {0}")]
     FileError(std::io::Error),
 
