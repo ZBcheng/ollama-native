aac50b769f48c87cacbfe89704795fded2126630
stackoverflower <bee0_0@icloud.com>
2025-03-04 17:34:47 +0800

    chore: import Responses in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,23 @@ pub mod config;
 pub mod error;
 pub mod ollama;
 
-pub use abi::Message;
 pub use error::OllamaError;
 pub use ollama::Ollama;
+
+pub use abi::Message;
+pub use abi::completion::{
+    chat::{ChatCompletionModelResponse, ChatCompletionResponse},
+    generate::{GenerateCompletionModelResponse, GenerateCompletionResponse},
+};
+pub use abi::version::VersionResponse;
+
+#[cfg(feature = "model")]
+pub use abi::model::{
+    create::CreateModelResponse, generate_embeddings::GenerateEmbeddingsResponse,
+    list_local::ListLocalModelsResponse, list_running::ListRunningModelsResponse,
+    pull::PullModelResponse, push::PushModelResponse, show_info::ShowModelInformationResponse,
+};
+
+#[cfg(feature = "model")]
+#[cfg(feature = "stream")]
+pub use abi::model::{pull::PullModelStreamingResponse, push::PushModelStreamingResponse};
